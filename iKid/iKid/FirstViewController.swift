72572690d90eb29fb1d77611d65b079be915f07d
StoneSolidCode <chesdude@gmail.com>
2018-10-24 02:03:50 -0700

    ikid initiated

---
+++ iKid/iKid/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iKid
+//
+//  Created by iguest on 10/23/18.
+//  Copyright © 2018 stonek9. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
